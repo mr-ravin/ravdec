e18b3514cc285043f789e1128b560ad28f6768ea
Ravin Kumar <16964978+mr-ravin@users.noreply.github.com>
2025-03-05 01:32:35 +0530

    version 3.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="ravdec",
-    version="3.3",
+    version="3.4",
     author="Ravin Kumar",
     author_email="mr.ravin_kumar@hotmail.com",  # Make sure this is correct
     description="Ravdec - Lossless Data Compression",
